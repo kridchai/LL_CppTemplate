d78f44ae563c19221e4e77b05eaaf0a8222ef8f0
YareYare <>
2021-04-11 10:43:39 +0000

    f

---
+++ makefile
@@ -0,0 +1,2 @@
+target: monster.cpp LL.cpp LL_test.cpp
+	g++ monster.cpp LL.cpp LL_test.cpp -o LL
--- monster.h
+++ monster.h
@@ -2,10 +2,10 @@
 class Godzilla{
 private:      
       int hp,potion;
-      string name;
+      std::string name;
       Godzilla* next;
 public:
-        Godzilla(int h = 100,int p = 100,string n = "unknow");
+        Godzilla(int h = 100,int p = 100,std::string n = "unknow");
         void show_Godzilla();
         void insert(Godzilla*&);
         Godzilla* move_next();
